1cdb44c8760873756c3b84a1c51dcc480cd15f8a
Geoffrey TESTELIN <geoffrey.testelin@gmail.com>
2020-08-09 14:38:50 +0200

    feat(errors): add more error examples

--- src/functions/log-something.spec.ts
+++ src/functions/log-something.spec.ts
@@ -1,34 +1,119 @@
 import { createMock } from "ts-auto-mock";
 import { logSomething } from "./log-something";
-import { Client, Presence } from "discord.js";
+import { Client, Presence, PresenceData } from "discord.js";
 
 describe(`logSomething()`, (): void => {
-  let setPresenceMock: jest.Mock<Promise<Presence>>;
-  let client: Client;
+  describe(`error with setPresenceMock and strong typing`, (): void => {
+    let setPresenceMock: jest.Mock<Promise<Presence>>;
+    let client: Client;
 
-  let consoleLogSpy: jest.SpyInstance;
+    let consoleLogSpy: jest.SpyInstance;
 
-  beforeEach((): void => {
-    setPresenceMock = jest
-      .fn()
-      .mockRejectedValue(new Error(`setPresence: error`));
-    client = createMock<Client>({
-      // Error here
-      user: {
-        setPresence: setPresenceMock,
-      },
+    beforeEach((): void => {
+      setPresenceMock = jest
+        .fn()
+        .mockRejectedValue(new Error(`setPresence: error`));
+      client = createMock<Client>({
+        // Error here
+        user: {
+          setPresence: setPresenceMock,
+        },
+      });
+
+      consoleLogSpy = jest.spyOn(console, `log`).mockImplementation();
     });
 
-    consoleLogSpy = jest.spyOn(console, `log`).mockImplementation();
+    it(`should log something`, (): void => {
+      expect.assertions(3);
+
+      logSomething();
+
+      expect(client).toStrictEqual(client);
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`something`);
+    });
   });
 
-  it(`should log something`, (): void => {
-    expect.assertions(3);
+  describe(`error with setPresenceMock and no typing`, (): void => {
+    let setPresenceMock: jest.Mock;
+    let client: Client;
 
-    logSomething();
+    let consoleLogSpy: jest.SpyInstance;
 
-    expect(client).toStrictEqual(client);
-    expect(consoleLogSpy).toHaveBeenCalledTimes(1);
-    expect(consoleLogSpy).toHaveBeenCalledWith(`something`);
+    beforeEach((): void => {
+      setPresenceMock = jest
+        .fn()
+        .mockRejectedValue(new Error(`setPresence: error`));
+      client = createMock<Client>({
+        // Error here
+        user: {
+          setPresence: setPresenceMock,
+        },
+      });
+
+      consoleLogSpy = jest.spyOn(console, `log`).mockImplementation();
+    });
+
+    it(`should log something`, (): void => {
+      expect.assertions(3);
+
+      logSomething();
+
+      expect(client).toStrictEqual(client);
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`something`);
+    });
+  });
+
+  describe(`error with setPresence fake data`, (): void => {
+    let client: Client;
+
+    let consoleLogSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      client = createMock<Client>({
+        // Error here
+        user: {
+          setPresence: (_data: PresenceData): Promise<Presence> =>
+            Promise.resolve(createMock<Presence>()),
+        },
+      });
+
+      consoleLogSpy = jest.spyOn(console, `log`).mockImplementation();
+    });
+
+    it(`should log something`, (): void => {
+      expect.assertions(3);
+
+      logSomething();
+
+      expect(client).toStrictEqual(client);
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`something`);
+    });
+  });
+
+  describe(`no error without setPresenceMock`, (): void => {
+    let client: Client;
+
+    let consoleLogSpy: jest.SpyInstance;
+
+    beforeEach((): void => {
+      client = createMock<Client>({
+        user: {},
+      });
+
+      consoleLogSpy = jest.spyOn(console, `log`).mockImplementation();
+    });
+
+    it(`should log something`, (): void => {
+      expect.assertions(3);
+
+      logSomething();
+
+      expect(client).toStrictEqual(client);
+      expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`something`);
+    });
   });
 });
